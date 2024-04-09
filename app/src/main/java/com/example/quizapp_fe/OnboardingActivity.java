60484d965ee8f9f431588ef9f10d5f14d848cf8f
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-04-09 20:07:44 +0700

    Fix Bug

--- app/src/main/java/com/example/quizapp_fe/OnboardingActivity.java
+++ app/src/main/java/com/example/quizapp_fe/OnboardingActivity.java
@@ -2,7 +2,10 @@ package com.example.quizapp_fe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class OnboardingActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class OnboardingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        TextView logInTextView = findViewById(R.id.onboardingLoginTextView);
+        logInTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OnboardingActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quizapp_fe/SignInActivity.java
+++ app/src/main/java/com/example/quizapp_fe/SignInActivity.java
@@ -2,7 +2,10 @@ package com.example.quizapp_fe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        ImageView backArrow = findViewById(R.id.signInBackArrowImgView);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this, OnboardingActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
