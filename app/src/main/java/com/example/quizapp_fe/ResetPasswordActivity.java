142bb742f41c6057437f1de1805875d4b8dc6248
NguyenHuynhPhucThinh <phuclam1005@gmail.com>
2024-04-10 17:24:05 +0700

    Update Reset Password Screen

--- app/src/main/java/com/example/quizapp_fe/ResetPasswordActivity.java
+++ app/src/main/java/com/example/quizapp_fe/ResetPasswordActivity.java
@@ -1,14 +1,31 @@
 package com.example.quizapp_fe;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class ResetPasswordActivity extends AppCompatActivity {
+    ImageView backArrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+        backArrow = findViewById(R.id.resetPasswordBackArrowImageView);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_normal);
+                backArrow.startAnimation(animation);
+                Intent intent = new Intent(ResetPasswordActivity.this , SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
