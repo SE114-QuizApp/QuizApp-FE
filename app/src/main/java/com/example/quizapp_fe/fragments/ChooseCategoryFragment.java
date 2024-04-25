effa236c5983b11e3233ce43390ff4fb53f12711
Minh Nhat <130208940+ngminhat187@users.noreply.github.com>
2024-04-26 02:48:18 +0700

    Create Navigation of Creator Quiz (#49)

--- app/src/main/java/com/example/quizapp_fe/fragments/ChooseCategoryFragment.java
+++ app/src/main/java/com/example/quizapp_fe/fragments/ChooseCategoryFragment.java
@@ -1,20 +1,52 @@
 package com.example.quizapp_fe.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.quizapp_fe.R;
+import com.example.quizapp_fe.activities.HomeActivity;
 
 public class ChooseCategoryFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_choose_category, container, false);
+
+        ImageView btnBack = view.findViewById(R.id.btnBackChooseCategory);
+
+        Button btnNext = view.findViewById(R.id.btnNextChooseCategory);
+
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requireActivity().finish();
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.creatorQuizMainContainerFrameLayout, new CreateQuizFragment());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return view;
     }
 }
