f0cbfd2924d219bd0163a4b2cc58d48bdaf6f3ed
NguyenHuynhPhucLam <106139520+NguyenHuynhPhucLam@users.noreply.github.com>
2024-05-18 15:09:34 +0700

    Fix Bug (#58)

--- app/src/main/java/com/example/quizapp_fe/adapters/QuizCardViewHolder.java
+++ app/src/main/java/com/example/quizapp_fe/adapters/QuizCardViewHolder.java
@@ -1,5 +1,7 @@
 package com.example.quizapp_fe.adapters;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -26,13 +28,22 @@ public class QuizCardViewHolder extends RecyclerView.ViewHolder {
         quizCardItemTitle = itemView.findViewById(R.id.quizCardItemTitleTextView);
         quizCardItemCreator = itemView.findViewById(R.id.quizCardItemCreatorTextView);
         quizCardItemStatus = itemView.findViewById(R.id.quizCardItemStatusTextView);
+        quizCardForwardArrow = itemView.findViewById(R.id.quizCardItemForwardArrow);
     }
 
-    public void bind(QuizCard quizCard) {
+    public void bind(QuizCard quizCard, Context context) {
         quizCardItemImageView.setImageResource(quizCard.getQuizCardImage());
         quizCardItemTitle.setText(quizCard.getTitleText());
         quizCardItemCreator.setText(quizCard.getCreatorText());
         quizCardItemStatus.setText(quizCard.getStatusText());
+
+        quizCardForwardArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(context.getApplicationContext(), R.anim.animation_normal);
+                quizCardForwardArrow.startAnimation(animation);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/quizapp_fe/fragments/UserFragment.java
+++ app/src/main/java/com/example/quizapp_fe/fragments/UserFragment.java
@@ -6,15 +6,23 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
 import com.example.quizapp_fe.R;
 import com.example.quizapp_fe.adapters.UserCardAdapter;
+import com.example.quizapp_fe.api.user.getAllUsers.GetAllUsersApi;
+import com.example.quizapp_fe.entities.User;
 import com.example.quizapp_fe.interfaces.UserCard;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserFragment extends Fragment {
 
@@ -28,15 +36,35 @@ public class UserFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_user, container, false);
         recyclerView = view.findViewById(R.id.userFragRecyclerView);
         userCardArrayList = new ArrayList<>();
-        userCardArrayList.add(new UserCard(R.drawable.img_lookup, "Phuc Lam", "phuclam@gmail.com"));
-        userCardArrayList.add(new UserCard(R.drawable.img_lookup, "Phuc Thinh", "phucthinh@gmail.com"));
-        userCardArrayList.add(new UserCard(R.drawable.img_lookup, "Phuc Lam", "phuclam@gmail.com"));
-        userCardArrayList.add(new UserCard(R.drawable.img_lookup, "Phuc Lam", "phuclam@gmail.com"));
-        userCardArrayList.add(new UserCard(R.drawable.img_lookup, "Phuc Lam", "phuclam@gmail.com"));
-        userCardArrayList.add(new UserCard(R.drawable.img_lookup, "Phuc Lam", "phuclam@gmail.com"));
-        userCardAdapter = new UserCardAdapter(view.getContext(), userCardArrayList);
-        recyclerView.setAdapter(userCardAdapter);
-        recyclerView.setLayoutManager(new GridLayoutManager(view.getContext(), 1));
+        callApi();
         return view;
+    }
+
+    public void callApi() {
+        GetAllUsersApi.getAPI(requireContext()).getAllUsers().enqueue(new Callback<ArrayList<User>>() {
+            @Override
+            public void onResponse(Call<ArrayList<User>> call, Response<ArrayList<User>> response) {
+                if(response.isSuccessful()){
+                    ArrayList<User> usersList = response.body();
+                    for (int i = 0; i < usersList.size(); i++) {
+                        userCardArrayList.add(new UserCard(
+                                usersList.get(i).getAvatar(),
+                                usersList.get(i).getUserName(),
+                                usersList.get(i).getMail()));
+                    }
+//                    userCardArrayList.add(new UserCard(usersList.get(0).getAvatar(),
+//                                                       usersList.get(0).getUserName(),
+//                                                       usersList.get(0).getMail()));
+                    userCardAdapter = new UserCardAdapter(requireContext(), userCardArrayList);
+                    recyclerView.setAdapter(userCardAdapter);
+                    recyclerView.setLayoutManager(new GridLayoutManager(requireContext(), 1));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<User>> call, Throwable t) {
+                Log.e("USERFRAG", "Call All User Api Fail");
+            }
+        });
     }
 }
