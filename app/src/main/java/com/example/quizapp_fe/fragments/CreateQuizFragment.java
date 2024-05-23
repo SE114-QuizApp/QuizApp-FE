2621c0916145f5511251203e9fa69a5eb838bcc0
Nguyen Minh Nhat <130208940+ngminhat187@users.noreply.github.com>
2024-05-23 12:08:40 +0700

    feat: create confirmation dialog and init create quiz api (#99)

--- app/src/main/java/com/example/quizapp_fe/fragments/CreateQuizFragment.java
+++ app/src/main/java/com/example/quizapp_fe/fragments/CreateQuizFragment.java
@@ -1,6 +1,8 @@
 package com.example.quizapp_fe.fragments;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -21,6 +23,7 @@ import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -33,10 +36,13 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.quizapp_fe.R;
 import com.example.quizapp_fe.activities.HomeActivity;
+import com.example.quizapp_fe.dialogs.ConfirmationDialog;
+import com.example.quizapp_fe.dialogs.LoadingDialog;
 import com.example.quizapp_fe.entities.Grade;
 import com.example.quizapp_fe.entities.Quiz;
 import com.example.quizapp_fe.models.CreateQuizViewModel;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -53,48 +59,14 @@ public class CreateQuizFragment extends Fragment {
     private EditText etQuizTags;
     private EditText etQuizDescription;
     private ImageView btnMoreIcon;
-
-    private final ActivityResultLauncher<String> mGetContent = registerForActivityResult(
-            new ActivityResultContracts.GetContent(),
-            new ActivityResultCallback<Uri>() {
-                @Override
-                public void onActivityResult(Uri uri) {
-                    try {
-                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), uri);
-                        quizBackgroundImage.setImageBitmap(bitmap);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-
-    private final ActivityResultLauncher<String> requestPermissionLauncher =
-            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-                    mGetContent.launch("image/*");
-                } else {
-                    Toast.makeText(getActivity(), "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
-                }
-            });
+    private LoadingDialog loadingDialog;
+    private Uri selectedImageUri;
+    private ConfirmationDialog confirmationDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_quiz, container, false);
-
-        quizBackgroundImage = view.findViewById(R.id.quizBackgroundImage);
-        Button btnChooseQuizBackgroundImage = view.findViewById(R.id.btnChooseQuizBackgroundImage);
-
-        btnChooseQuizBackgroundImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
-                } else {
-                    mGetContent.launch("image/*");
-                }
-            }
-        });
 
         etQuizName = view.findViewById(R.id.etQuizName);
         switchPublicQuiz = view.findViewById(R.id.switchPublicQuiz);
@@ -103,7 +75,13 @@ public class CreateQuizFragment extends Fragment {
         etQuizDescription = view.findViewById(R.id.etQuizDescription);
         tvGrade = view.findViewById(R.id.tvGrade);
         gradeContainer = view.findViewById(R.id.gradeContainer);
+        btnMoreIcon = view.findViewById(R.id.btnMoreIcon);
+        ImageView btnBack = view.findViewById(R.id.btnBackCreateQuiz);
+        Button btnAddQuestions = view.findViewById(R.id.btnAddQuestions);
+        quizBackgroundImage = view.findViewById(R.id.quizBackgroundImage);
+        Button btnChooseQuizBackgroundImage = view.findViewById(R.id.btnChooseQuizBackgroundImage);
 
+        confirmationDialog = new ConfirmationDialog(requireContext());
 
         // get the quiz object from the ViewModel
         createQuizViewModel = new ViewModelProvider(requireActivity()).get(CreateQuizViewModel.class);
@@ -122,39 +100,49 @@ public class CreateQuizFragment extends Fragment {
             }
 
             // set the background image of the quiz if it exists
-            if (!quiz.getBackgroundImage().isEmpty()) {
-                byte[] imageBytes = Base64.decode(quiz.getBackgroundImage(), Base64.DEFAULT);
-                Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-                quizBackgroundImage.setImageBitmap(bitmap);
+            if (quiz.getBackgroundImage() != null && !quiz.getBackgroundImage().isEmpty()) {
+                byte[] decodedString = Base64.decode(quiz.getBackgroundImage(), Base64.DEFAULT);
+                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                quizBackgroundImage.setImageBitmap(decodedByte);
             }
         }
 
         gradeContainer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showGradeMenu(v);
+                PopupMenu popupMenu = new PopupMenu(getActivity(), v);
+                popupMenu.getMenuInflater().inflate(R.menu.grade_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        tvGrade.setText(item.getTitle());
+                        return true;
+                    }
+                });
+                popupMenu.show();
             }
         });
-
-        btnMoreIcon = view.findViewById(R.id.btnMoreIcon);
 
         btnMoreIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 PopupMenu popupMenu = new PopupMenu(getActivity(), v);
-                popupMenu.getMenuInflater().inflate(R.menu.popup_menu_more_icon_create_quiz_fragment, popupMenu.getMenu());
+                popupMenu.getMenuInflater().inflate(R.menu.more_options_create_quiz_menu, popupMenu.getMenu());
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem item) {
                         switch (item.getTitle().toString()) {
                             case "Discard changes":
-//                                createQuizViewModel.clearQuiz();
+                                confirmationDialog.show("Discard changes", "Are you sure you want to exit creating quiz?", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Intent intent = new Intent(getActivity(), HomeActivity.class);
+                                        startActivity(intent);
+                                        getActivity().finish();
 
-                                Intent intent = new Intent(getActivity(), HomeActivity.class);
-                                startActivity(intent);
-                                getActivity().finish();
-
-                                Toast.makeText(getActivity(), "Exit creating quiz", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(getActivity(), "Exit creating quiz", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
 
                                 return true;
                             default:
@@ -166,10 +154,6 @@ public class CreateQuizFragment extends Fragment {
                 popupMenu.show();
             }
         });
-
-
-        ImageView btnBack = view.findViewById(R.id.btnBackCreateQuiz);
-        Button btnAddQuestions = view.findViewById(R.id.btnAddQuestions);
 
         btnAddQuestions.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -199,30 +183,31 @@ public class CreateQuizFragment extends Fragment {
             }
         });
 
-        return view;
-    }
-
-    // add values to the grade menu
-    private void showGradeMenu(View v) {
-        PopupMenu popupMenu = new PopupMenu(getActivity(), v);
-        for (int i = 1; i <= 12; i++) {
-            popupMenu.getMenu().add(String.valueOf(i));
-        }
-
-        // add the "All" option to the menu
-        popupMenu.getMenu().add("All");
-        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+        btnChooseQuizBackgroundImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                tvGrade.setText(item.getTitle());
-                return true;
+            public void onClick(View v) {
+                checkAndRequestPermission();
             }
         });
-        popupMenu.show();
+
+        return view;
     }
 
     // update the quiz object
     private void updateQuiz() {
+        if (selectedImageUri != null) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(requireActivity().getContentResolver(), selectedImageUri);
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                byte[] byteArray = outputStream.toByteArray();
+                String encodedImage = Base64.encodeToString(byteArray, Base64.DEFAULT);
+                quiz.setBackgroundImage(encodedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         quiz.setName(etQuizName.getText().toString().trim());
         quiz.setPublic(switchPublicQuiz.isChecked());
         quiz.setGrade(new Grade(tvGrade.getText().toString().trim()));
@@ -231,25 +216,12 @@ public class CreateQuizFragment extends Fragment {
             quiz.setPointsPerQuestion(Integer.parseInt(etQuizPointsPerQuestion.getText().toString().trim()));
         }
 
-
         // convert the tags from UI string to a list of tags
         String tagsString = etQuizTags.getText().toString().trim();
         ArrayList<String> tags = new ArrayList<>(Arrays.asList(tagsString.split(", ")));
         quiz.setTags(tags);
 
         quiz.setDescription(etQuizDescription.getText().toString().trim());
-
-        // set the background image of the quiz (error here)
-        if (quizBackgroundImage.getDrawable() != null) {
-//            Bitmap bitmap = ((BitmapDrawable) quizBackgroundImage.getDrawable()).getBitmap();
-//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-//            byte[] imageBytes = baos.toByteArray();
-//            String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
-//            quiz.setBackgroundImage(imageString);
-        } else {
-            quiz.setBackgroundImage("");
-        }
 
         createQuizViewModel.setQuiz(quiz);
     }
@@ -275,4 +247,44 @@ public class CreateQuizFragment extends Fragment {
 
         return isValid;
     }
+
+    private void checkAndRequestPermission() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+        } else {
+            pickImage();
+        }
+    }
+
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickImageLauncher.launch(intent);
+    }
+
+    ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        selectedImageUri = result.getData().getData();
+                        quizBackgroundImage.setImageURI(selectedImageUri);
+                    }
+                }
+            }
+    );
+
+    ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean isGranted) {
+                    if (isGranted) {
+                        pickImage();
+                    } else {
+                        Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+    );
 }
